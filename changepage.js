4ad5399f7ff0ac4e4bd6fb9a5122cdba09cbf1d1
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2023-09-05 19:11:39 +0900

    배너 화면 전환 버튼 기능 구현 완성

--- changepage.js
+++ changepage.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     // 선언
+    const main_btn = document.getElementById('main_btn');
     const animal_btn = document.getElementById('animal_btn');
     const personalised_btn = document.getElementById('personalised_btn');
     const book_btn = document.getElementById('book_btn');
     const mypage_btn = document.getElementById('mypage_btn');
     
     //클릭시 이벤트 (화면 전환)
+    main_btn.addEventListener('click', () => {
+        location.href = 'http://127.0.0.1:5500/main.html'; 
+    });
     animal_btn.addEventListener('click', () => {
         location.href = 'http://127.0.0.1:5500/animal.html'; 
     });
